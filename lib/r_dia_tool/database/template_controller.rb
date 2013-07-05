98063f9458c26d88af4d7d2b7f3f67727e0b21fd
Chris Lynch <chrislynch42@yahoo.com>
2013-07-05 15:54:14 -0400

    Updating TemplateController

--- lib/r_dia_tool/database/template_controller.rb
+++ lib/r_dia_tool/database/template_controller.rb
@@ -2,7 +2,13 @@ module RDiaTool
   module Database
 
     class TemplateController
-      attr_accessor :template_directory, :dia_file
+      attr_reader :template, :dia_xml, :database_configuration_file
+
+      def initialize(template,dia_xml,database_configuration_file)
+        @template=template
+        @dia_xml=dia_xml
+        @database_configuration_file=database_configuration_file
+      end
 
     end
   end
--- spec/template_controller_spec.rb
+++ spec/template_controller_spec.rb
@@ -7,13 +7,18 @@ module RDiaTool
     describe TemplateController do
 
       describe "Class" do
-        it "TemplateController should have attribute 'template_directory' defined" do
-          TemplateController.instance_methods(false).include?(:template_directory).should be_true
+        it "TemplateController should have attribute 'template' defined" do
+          TemplateController.instance_methods(false).include?(:template).should be_true
         end
 
-        it "TemplateController should have attribute 'dia_file' defined" do
-          TemplateController.instance_methods(false).include?(:dia_file).should be_true
+        it "TemplateController should have attribute 'dia_xml' defined" do
+          TemplateController.instance_methods(false).include?(:dia_xml).should be_true
         end
+
+        it "TemplateController should have attribute 'database_configuration_file' defined" do
+          TemplateController.instance_methods(false).include?(:database_configuration_file).should be_true
+        end
+        
 
       end
 
