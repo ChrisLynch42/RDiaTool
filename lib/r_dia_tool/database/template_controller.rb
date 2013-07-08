1ddb78e81780e561af942f2bad15bf5f6b369d67
Chris Lynch <chrislynch42@yahoo.com>
2013-07-08 14:40:55 -0400

    Added database connectivity to TemplateController

--- lib/r_dia_tool/database/template_controller.rb
+++ lib/r_dia_tool/database/template_controller.rb
@@ -1,13 +1,37 @@
+require 'active_record'
+require 'yaml'
+require 'logger'
+
 module RDiaTool
   module Database
 
     class TemplateController
-      attr_reader :template, :dia_xml, :database_configuration_file
+      attr_reader :template, :dia_xml_file, :database_configuration
 
-      def initialize(template,dia_xml,database_configuration_file)
+      def initialize(dia_xml_file,template,database_configuration)
         @template=template
-        @dia_xml=dia_xml
-        @database_configuration_file=database_configuration_file
+        @dia_xml=dia_xml_file
+        @database_configuration=database_configuration
+        unless database_configuration.nil? || database_configuration.kind_of?(Hash)
+          raise Exception.new("database configuration is not a hash")
+        end
+      end
+
+      def connect
+        unless FileTest.exist?(@database_configuration['database'])
+          raise Exception.new("Database " + @database_configuration['database'] + " does not exist.")
+        end        
+        ActiveRecord::Base.establish_connection(@database_configuration)
+        ActiveRecord::Base.connection.tables
+        unless ActiveRecord::Base.connected?
+          raise Exception.new("Database Connection failed!")
+        end        
+      end
+
+
+      def database_connected?
+        #puts ActiveRecord::Base.methods(true) 
+        ActiveRecord::Base.connected?
       end
 
     end
--- spec/template_controller_spec.rb
+++ spec/template_controller_spec.rb
@@ -11,22 +11,34 @@ module RDiaTool
           TemplateController.instance_methods(false).include?(:template).should be_true
         end
 
-        it "TemplateController should have attribute 'dia_xml' defined" do
-          TemplateController.instance_methods(false).include?(:dia_xml).should be_true
+        it "TemplateController should have attribute 'dia_xml_file' defined" do
+          TemplateController.instance_methods(false).include?(:dia_xml_file).should be_true
         end
 
-        it "TemplateController should have attribute 'database_configuration_file' defined" do
-          TemplateController.instance_methods(false).include?(:database_configuration_file).should be_true
+        it "TemplateController should have attribute 'database_configuration' defined" do
+          TemplateController.instance_methods(false).include?(:database_configuration).should be_true
         end
         
 
       end
 
       describe "Instance" do
-        before(:each) do 
+        before(:each) do
+          dia_file=File.dirname(__FILE__) + '/test.dia' 
+          database_file=File.dirname(__FILE__) + '/test_database/development.sqlite3' 
+          database_config= { 'adapter' => 'sqlite3', 'database' => database_file}
+          @template_controller = TemplateController.new(dia_file,'RailsModel',database_config)
         end
 
+        it "it should return false when it receives the 'database_connected?' message" do
+          @template_controller.database_connected?().should be_false
+        end
 
+        it "it should return true when it receives the 'database_connected?' message" do
+          @template_controller.connect()
+          @template_controller.database_connected?().should be_true
+        end
+        
       end
     end
   end
