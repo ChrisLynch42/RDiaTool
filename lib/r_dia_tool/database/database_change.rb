add5b4729aa47d0a04636ca06cfe6e67a4d7a278
Chris Lynch <chrislynch42@yahoo.com>
2013-08-20 14:48:45 -0400

    Made sure that attributes are initialized as hashes.

--- lib/r_dia_tool/database/database_change.rb
+++ lib/r_dia_tool/database/database_change.rb
@@ -4,6 +4,12 @@ module RDiaTool
     class DatabaseChange
       attr_accessor :add, :remove, :modify, :table
 
+      def initialize()
+        @add = Hash.new()
+        @remove = Hash.new()
+        @modify = Hash.new()
+      end
+
     end
   end
 end
