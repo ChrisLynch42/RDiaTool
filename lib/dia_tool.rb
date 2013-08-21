1a72116c3c637bda2e6a5ff802ddf6775aa1df55
Chris Lynch <chrislynch42@yahoo.com>
2013-08-21 17:14:27 -0400

    Added error checking to the command line options

--- lib/dia_tool.rb
+++ lib/dia_tool.rb
@@ -3,14 +3,51 @@ require 'rubygems'
 require 'thor'
 
 class DiaTool < Thor
-  desc "model_generate FILE", "Generate code files from a Dia database diagram."
+  
+  TEMPLATES = ['RailsModel']
+
+
+  desc "database_generate FILE", "Generate code files from a Dia database diagram."
   method_option :database_configuration, :aliases => "-c", :type => :string, :desc => "An ActiveRecord datase configuration in YAML format."
   method_option :rails_dir, :aliases => "-r", :type => :string, :desc => "Root directory of a rails application.  If this option is used it will override the --database_configuration option."
-  method_option :template, :aliases => "-t", :type => :string, :required => true, :desc => "This is the template used to generate the source code"
+  method_option :template, :aliases => "-t", :type => :string, :required => true, :desc => "This is the template used to generate the source code.  Currently only 'RailsModel' template is available."
   method_option :target_dir, :aliases => "-d", :type => :string, :required => true, :desc => "This is the directory where you would like the generated code placed."  
-  def model_build(dia_database_diagram)
-    puts 'finish me'
-    puts options[:rails_dir]
+  def database_generate(dia_database_diagram)
+    has_error=false
+    unless File.exist?(dia_database_diagram)
+      puts "Dia database diagram does not exist"
+      has_error=true
+    end
+    unless Dir.exist?(options[:target_dir])
+      puts "Target directory does not exist"
+      has_error=true
+    end
+    unless Dir.exist?(options[:rails_dir])
+      puts "Rails directory does not exist"
+      has_error=true
+    end
+    unless File.exist?(options[:database_configuration])
+      puts "Database configuration file does not exist"
+      has_error=true
+    end    
+    if options[:template].nil?
+      puts "You must select a template using --template"      
+      has_error=true
+    else
+      unless TEMPLATES.include?(options[:template])
+        puts "You have not selected a valid template.  'RailsModel' is the only valid template."
+        has_error=true
+      end
+    end
+    if options[:database_configuration].nil? && options[:rails_dir].nil?
+      puts "Either --database_configuration or --rails_dir must be used."
+      return
+    end
+    if has_error
+     return
+    else
+      #instantiate template controller
+    end
   end
 end
 
