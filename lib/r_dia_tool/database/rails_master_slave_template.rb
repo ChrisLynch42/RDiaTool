82b9d65eaf17c7673cff70308c4badbd69dabef1
Chris Lynch <chrislynch42@yahoo.com>
2013-11-01 15:03:34 -0400

    In the process of adding genration of controllers to master slave template

--- lib/r_dia_tool/database/rails_master_slave_template.rb
+++ lib/r_dia_tool/database/rails_master_slave_template.rb
@@ -16,6 +16,61 @@ module RDiaTool
 
       private
 
+
+      def modify_controller(table_name)
+        controller_file_name = table_name.downcase + "_controller.rb"
+        template_variables = Hash.new()
+        template_variables['single'] = table_name.downcase.singularize
+        template_variables['cap_single'] = table_name.singularize.capitalize
+        template_variables['plural'] = table_name.downcase
+        template_variables['cap_plural'] = table_name.downcase.capitalize
+        template_variables['table_name']=table_name
+
+        if File.exist?(@controller_directory + "/" + controller_file_name)
+          content = load_template(@controller_directory + '/' + controller_file_name)
+          modify_controller_section(content,'index',template_variables)
+          modify_controller_section(content,'show',template_variables)
+          modify_controller_section(content,'edit',template_variables)
+          modify_controller_section(content,'new',template_variables)
+          modify_controller_section(content,'create',template_variables)
+          modify_controller_section(content,'update',template_variables)
+          modify_controller_section(content,'destroy',template_variables)
+        else
+          content = erb_output(@base_directory + "/controller_master.erb",template_variables)
+        end
+        write_template_results(@controller_directory + '/' + controller_file_name,content)        
+      end
+
+
+      def modify_controller_section(content,section,template_variables)
+        if content.nil? || content.strip().length < 1
+          raise "Empty content!"
+        end
+
+        template_name_prepare = "controller_#{section.downcase}_prepare.rb"
+        template_name_send = "controller_#{section.downcase}_send.rb"
+        prepare_content = get_template_results(template_name_prepare,template_variables)
+        send_content = get_template_results(template_name_send,template_variables)
+        unless prepare_content.nil?
+          re = /^\s*### DO NOT EDIT BELOW PREPARE #{section.upcase}.*### DO NOT EDIT BELOW PREPARE #{section.upcase}[ \S]*$/m
+          unless content.index(re).nil?
+            content.sub!(re,prepare_content)
+          end
+        end
+        unless send_content.nil?
+          re = /^\s*### DO NOT EDIT BELOW SEND #{section.upcase}.*### DO NOT EDIT BELOW SEND #{section.upcase}[ \S]*$/m
+          unless content.index(re).nil?
+            content.sub!(re,send_content)
+          end
+        end        
+      end
+
+      def get_template_results(template_name,template_variables)
+        return_value = nil
+        Dir.glob(@base_directory + "/#{template_name}").each do | file_name |
+          return_value = erb_output(file_name,template_variables)
+        end
+      end
     end
   end
 end
